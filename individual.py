be41c942675b432be8e9cd8aeed3014a410f0c45
Leonardo Schimpf Machado <leonardo.schimpf@gmail.com>
2018-09-25 15:10:56 -0300

    Repetição não permitida no genetico

--- individual.py
+++ individual.py
@@ -14,15 +14,22 @@ class Individual:
 
     def generate_random_genes(self, heuristic, vocabulary):
         for i in range(self.size):
-            self.genes.append(sample(vocabulary, 1)[0])
+            random_gene = self.generate_random_gene(vocabulary)
+            self.genes.append(random_gene)
         self.fitness = self.calculate_fitness(heuristic)
+
+    def generate_random_gene(self, vocabulary):
+        candidate_gene = sample(vocabulary, 1)[0]
+        while (candidate_gene in self.genes):
+            candidate_gene = sample(vocabulary, 1)[0]
+        return candidate_gene
 
     def calculate_fitness(self, heuristic):
         return heuristic.value_of(self.genes)
 
     def mutate(self, heuristic, vocabulary):
         mutation_index = randint(0, len(self.genes) - 1)
-        new_word = sample(vocabulary, 1)[0]
-        self.genes[mutation_index] = new_word
+        random_gene = self.generate_random_gene(vocabulary)
+        self.genes[mutation_index] = random_gene
         self.fitness = self.calculate_fitness(heuristic)
 
