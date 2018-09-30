ebd72fb33de92fa90461c6812e297c37f841c1a3
arthurFerraz9 <arthurmf9@gmail.com>
2018-09-30 10:36:01 -0300

    Organiza em pastas

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from A_Star import A_Star
-from Problem import SyntaxProblem
-from heuristic import Heuristic
-from genetic import Genetic
-import text_utils
+from Algorithms.A_Star import A_Star
+from Problems.SyntaxProblem import SyntaxProblem
+from Evaluation.heuristic import Heuristic
+from Algorithms.genetic import Genetic
+from Utils import text_utils
 
 print("Which search do you want?")
 print("1. A*")
@@ -11,11 +11,11 @@ option = int(input())
 problem = None
 
 if option == 1:
-    vocabulary = text_utils.import_text("GameOfThrones")
-    heuristic = Heuristic("GameOfThronesClassifier")
+    vocabulary = text_utils.import_text("Texts/GameOfThrones")
+    heuristic = Heuristic("Classifiers/GameOfThronesClassifier")
     problem = A_Star(heuristic, SyntaxProblem(6), vocabulary)
 elif option == 2:
-    vocabulary = text_utils.genetic_import_text("ThreeLittlePigs")
-    heuristic = Heuristic("ThreeLittlePigsClassifier", "ThreeLittlePigs")
+    vocabulary = text_utils.genetic_import_text("Texts/ThreeLittlePigs")
+    heuristic = Heuristic("Classifiers/ThreeLittlePigsClassifier", "Texts/ThreeLittlePigs")
     problem = Genetic(heuristic, SyntaxProblem(6), vocabulary)
 problem.execute()
